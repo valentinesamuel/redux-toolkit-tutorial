713d26b75e56995b810f72b81040abef5ec0c172
valentinesamuel <valentinesamuel2580@gmail.com>
2022-05-15 15:13:17 +0100

    FEAT: Added addition and subtraction by amount and reset functionality

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,7 +1,21 @@
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import {
+  increment,
+  decrement,
+  reset,
+  incrementByAmount,
+  decrementByAmount,
+} from "./counterSlice";
+import { useState } from "react";
 
 const Counter = () => {
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const [decrementAmount, setDecrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset(0));
+  };
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
   return (
@@ -11,6 +25,18 @@ const Counter = () => {
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
       </div>
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <button onClick={() => dispatch(incrementByAmount(addValue))}>
+        Add Amount
+      </button>
+      <button onClick={() => dispatch(decrementByAmount(addValue))}>
+        Subtract Amount
+      </button>
+      <button onClick={resetAll}>Reset</button>
     </section>
   );
 };
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -14,8 +14,23 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.count -= 1;
     },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+    decrementByAmount: (state, action) => {
+      state.count -= action.payload;
+    },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const {
+  increment,
+  decrement,
+  decrementByAmount,
+  incrementByAmount,
+  reset,
+} = counterSlice.actions;
 export default counterSlice.reducer;
